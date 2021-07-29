1873c6be397cc0f1c5a2f8d4cf10b0c82b594f36
Mahammad <devmahammad@gmail.com>
2021-07-29 22:50:20 +0400

    fixed login bug

--- user/views.py
+++ user/views.py
@@ -1,47 +1,54 @@
-from django.shortcuts import render,redirect
-from .forms import UserRegisterForm ,UserLoginForm
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm, UserLoginForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login,authenticate,logout
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import authenticate, logout
+
 from django.contrib import messages
 
+
 def register(request):
-    
 
-        form = UserRegisterForm(request.POST or None)
-        if form.is_valid():
-             form.save()            
-             messages.success(request,"Successfully signed up")
-             return redirect("index")
+    form = UserRegisterForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        messages.success(request, "Successfully signed up")
+        return redirect("index")
 
-        context ={                       
-        "form":form
-     }
-        return render(request,"register.html",context)  
-    
+    context = {
+        "form": form
+    }
+    return render(request, "register.html", context)
+
+
 def login(request):
     form = UserLoginForm(request.POST or None)
     context = {
 
-        "form":form
+        "form": form
     }
 
     if form.is_valid():
+        print("VALIDATED...")
         username = form.cleaned_data.get("username")
         password = form.cleaned_data.get("password")
+        print(f"Username: ${username}")
+        print(f"Password: ${password}")
 
-        user = authenticate(username = username, password = password)
-
+        user = authenticate(username=username, password=password)
+        print(user)
         if(user is None):
             messages.info(request, "Incorrect username or password")
-            return render(request, "login.html",context)
+            return render(request, "login.html", context)
 
         messages.info(request, "Succesfully logged in")
+        auth_login(request, user)
         return redirect(("index"))
-        login(request,user)
 
-    return render(request,"login.html",context)
+    return render(request, "login.html", context)
 
-def log_out(request):    
+
+def log_out(request):
     logout(request)
     messages.success(request, "Successfully signed out")
 
